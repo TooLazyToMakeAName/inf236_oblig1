55ec6eb43afe68081b3ea1f673e96c910b08cf1e
TooLazyToMakeAName <30878599+TooLazyToMakeAName@users.noreply.github.com>
2019-02-19 13:24:14 -0800

    Update main.c

--- 2-Parallel/main.c
+++ 2-Parallel/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <string.h>
+#include <time.h>	
 
 struct Automa {
     int* initState;
@@ -101,7 +102,9 @@ int main(int argvc, char** argv) {
     MPI_Status receiveStatusEndElement;
     unsigned int t;
     sscanf(argv[3], "%d", &t);
-    if (rank == 0) {
+    clock_t begin;
+     if (rank == 0) {
+	begin = clock();
         automa = pasreInitState(binaryString);
         fx = getFunctionFromFile(fxFile);
         fx[2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2] = automa.stateLengths;
@@ -116,14 +119,24 @@ int main(int argvc, char** argv) {
     int seqBuffExtendedLength =
             sizeof(int) * (sideLength) * (sideHigth + 2);        //adds buffer space for the two odd elemetes
     int* seqBuffExtended = malloc(
-            seqBuffExtendedLength);                         //remember that everyting is now 1 index (ehh)
+            seqBuffExtendedLength);                      
     int* placeHolder = malloc(seqBuffExtendedLength);
     int* seqBuff = &seqBuffExtended[sideLength];
     int* bitSequence = malloc(sizeof(int) * 9);
     MPI_Scatter(automa.initState, sideHigth * sideLength, MPI_INT, seqBuff, sideHigth * sideLength, MPI_INT, 0,
                 MPI_COMM_WORLD);
 
+    int p = t/100;
+    int bar = 0;
     for (int i = 0; i < t; ++i) {
+	if(rank==0){
+	    if((i-bar)> p){
+		printf("=");
+		fflush(stdout);
+		bar = i;
+		}
+	}
+
         MPI_Isend(&seqBuff[0], sideLength, MPI_INT, mod(rank - 1, comm_sz), 0, MPI_COMM_WORLD,
                   &sendRequesStartElement);
         MPI_Isend(&seqBuff[(sideHigth - 1) * sideLength], sideLength, MPI_INT, mod(rank + 1, comm_sz), 1,
@@ -132,7 +145,6 @@ int main(int argvc, char** argv) {
                  &receiveStatusStartElement);
         MPI_Recv(&seqBuffExtended[(sideLength * (sideHigth + 1))], sideLength, MPI_INT, mod(rank + 1, comm_sz),
                  0, MPI_COMM_WORLD, &receiveStatusEndElement);
-
 
         for (int y = 1; y < sideHigth + 1; ++y) {
             for (int x = 0; x < sideLength; ++x) {
@@ -154,18 +166,21 @@ int main(int argvc, char** argv) {
         placeHolder = seqBuffExtended;
         seqBuffExtended = temp;
         seqBuff = &seqBuffExtended[sideLength];
-        //}
 
-        MPI_Gather(seqBuff, sideLength*sideHigth, MPI_INT, automa.initState, sideHigth*sideLength, MPI_INT, 0, MPI_COMM_WORLD);
-        if (rank == 0) {
-            print(automa.initState, automa.stateLengths);
+            }
+
+    MPI_Gather(seqBuff, sideLength*sideHigth, MPI_INT, automa.initState, sideHigth*sideLength, MPI_INT, 0, MPI_COMM_WORLD);
+if (rank == 0) {
+//            print(automa.initState, automa.stateLengths);
+	    clock_t end = clock();
+	    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	    printf("\n%.6f\n", time_spent);
+ 
         }
-
-    }
-
-    MPI_Finalize();
     free(seqBuffExtended);
     free(fx);
 
+       //*/
+    MPI_Finalize();
     return 0;
 }
